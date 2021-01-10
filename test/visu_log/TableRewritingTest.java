feda059f5bab03d0edb49e0bc2b7166397b3898f
Georgij Grinshpun <georgi.grinshpun@rwth-aachen.de>
2021-01-10 17:33:16 +0100

    another merging example with a main node

--- test/visu_log/TableRewritingTest.java
+++ test/visu_log/TableRewritingTest.java
@@ -69,4 +69,65 @@ class TableRewritingTest {
         }
 
     }
+
+    @Test
+    void compressTable2() {
+
+        Commit initial = TestCommit.createCommit("A");
+        Commit commit1 = TestCommit.createCommit("B", initial);
+        Commit commit2 = TestCommit.createCommit("C", commit1);
+
+        Commit merge1 = TestCommit.createCommit("m1", initial, commit1);
+        Commit merge2 = TestCommit.createCommit("m2", initial, commit1);
+
+        {
+            Column c1 = new Column();
+            Column c2 = new Column();
+            Column c3 = new Column();
+
+            CommitStorage.newEntryForParent(commit2, commit1, c1, TypeOfBackReference.NO, 0);
+
+            CommitStorage.newEntryForParent(merge1, initial, c2, TypeOfBackReference.NO, 1);
+            CommitStorage.newEntryForParent(merge1, commit1, c1, TypeOfBackReference.NO, 1);
+
+            CommitStorage.newEntryForParent(merge2, initial, c3, TypeOfBackReference.NO, 2);
+            CommitStorage.newEntryForParent(merge2, commit1, c1, TypeOfBackReference.NO, 2);
+
+            CommitStorage.newEntryForParent(commit1, initial, c1, TypeOfBackReference.YES, 3);
+
+            CommitStorage.newEntryForParent(initial, null, c1, TypeOfBackReference.YES, 4);
+            CommitStorage.newEntryBackReferenceWithoutParent(initial, c2, 4);
+            CommitStorage.newEntryBackReferenceWithoutParent(initial, c3, 4);
+
+            List<Column> columns = List.of(c1, c2, c3);
+            ColumnsSorterTest.validateAllColumns(columns);
+            testHelper.addResults(TableCreator.createTableFromDroppingColumns(List.of(c1, c2, c3)));
+        }
+
+        {
+            Column c1 = new Column();
+            Column c2 = new Column();
+            Column c3 = new Column();
+
+            CommitStorage.newEntryForParent(commit2, commit1, c1, TypeOfBackReference.NO, 0);
+
+            CommitStorage.newEntryForParent(merge1, initial, c2, TypeOfBackReference.NO, 1);
+            CommitStorage.newEntryForParent(merge1, commit1, c1, TypeOfBackReference.NO, 1);
+
+            CommitStorage.newEntryForParent(merge2, initial, c3, TypeOfBackReference.NO, 2);
+            CommitStorage.newEntryForParent(merge2, commit1, c1, TypeOfBackReference.NO, 2);
+
+            CommitStorage.newEntryForParent(commit1, initial, c1, TypeOfBackReference.YES, 3);
+
+            CommitStorage.newEntryForParent(initial, null, c1, TypeOfBackReference.YES, 4);
+            CommitStorage.newEntryBackReferenceWithoutParent(initial, c2, 4);
+            CommitStorage.newEntryBackReferenceWithoutParent(initial, c3, 4);
+
+            List<Column> columns = List.of(c1, c2, c3);
+            ColumnsSorterTest.validateAllColumns(columns);
+            List<List<TableEntry>> tableFromDroppingColumns = TableCreator.createTableFromDroppingColumns(List.of(c1, c2, c3));
+            TableRewriting.compressTable(tableFromDroppingColumns);
+            testHelper.addResults(tableFromDroppingColumns);
+        }
+    }
 }
